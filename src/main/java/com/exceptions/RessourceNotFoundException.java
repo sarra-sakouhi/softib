82ed4ecb89489e7a83f8a1a71a28111e6e9d799b
SarraSako <sarrasakouhi93@gmail.com>
2021-06-20 01:56:39 +0100

    SI-8: Implementation_de_Communication_Controllers

---
+++ src/main/java/com/exceptions/RessourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.exceptions;
+
+public class RessourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public RessourceNotFoundException(String message) {
+		super(message);
+	}
+}

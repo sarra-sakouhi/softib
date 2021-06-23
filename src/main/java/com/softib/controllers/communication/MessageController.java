8fa781969b6925400259b32bfdbd91305ae7ad65
SarraSako <sarrasakouhi93@gmail.com>
2021-06-23 05:10:15 +0100

    SI-8 : Implementation_de_Communication_Controller_Service

---
+++ src/main/java/com/softib/controllers/communication/MessageController.java
@@ -0,0 +1,62 @@
+package com.softib.controllers.communication;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.softib.entities.communication.Message;
+import com.softib.services.communication.IMessageService;
+
+@RestController
+@RequestMapping("/softib/com/")
+public class MessageController {
+	@Autowired
+	private IMessageService messageService;
+
+	@GetMapping("/Messages")
+	public List<Message> getAllMessages(){
+
+		return messageService.getAllMessages();
+	}
+
+	@PostMapping("/Messages")
+	public Message createMessage(@RequestBody Message message) {
+
+		return messageService.createMessage(message);
+	}
+
+	@GetMapping("/Messages/{id}")
+	public ResponseEntity<Message> getMessageById(@PathVariable long id) {
+
+		Message message=messageService.getMessageById(id);
+		return ResponseEntity.ok(message);
+	}
+
+	@PutMapping("/Messages/{id}")
+	public ResponseEntity<Message> updateMessage(@PathVariable long id, @RequestBody Message messageWithUpdates){
+
+		Message updatedMessage=messageService.updateMessage(id,messageWithUpdates);
+		return ResponseEntity.ok(updatedMessage);
+	}
+
+	@DeleteMapping("/Messages/{id}")
+	public ResponseEntity<Map<String,Boolean>> deleteMessage(@PathVariable long id){
+
+		messageService.deleteMessage(id);
+		Map<String,Boolean> response=new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return ResponseEntity.ok(response);
+
+	}
+}
---
+++ src/main/java/com/softib/services/communication/MessageService.java
@@ -0,0 +1,55 @@
+package com.softib.services.communication;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.softib.entities.communication.Message;
+import com.softib.exceptions.RessourceNotFoundException;
+import com.softib.repositories.communication.IMessageRepository;
+
+@Service
+public class MessageService {
+	@Autowired
+	private IMessageRepository messageRepository;
+	
+	private static final String RESSOURCE_NOT_FOUND_MSG="Message not found with id ";
+
+	public List<Message> getAllMessages() {
+
+	return messageRepository.findAll();
+
+	}
+
+	public Message createMessage(Message message) {
+
+	return messageRepository.save(message);
+
+	}
+
+	public Message getMessageById(long id) {
+
+	return messageRepository.findById(id)
+	.orElseThrow(() -> new RessourceNotFoundException(RESSOURCE_NOT_FOUND_MSG + id));
+	}
+
+	public Message updateMessage(long id, Message messageWithUpdates) {
+
+		Message message = messageRepository.findById(id)
+				.orElseThrow(() -> new RessourceNotFoundException(RESSOURCE_NOT_FOUND_MSG + id));
+
+		Message.setMessage(messageWithUpdates.getMessage());
+		Message updatedMessage = messageRepository.save(message);
+		return updatedMessage;
+	}
+
+	public void deleteMessage(long id) {
+
+	Message message = messageRepository.findById(id)
+	.orElseThrow(() -> new RessourceNotFoundException(RESSOURCE_NOT_FOUND_MSG + id));
+
+	messageRepository.delete(message);
+
+	}
+}
